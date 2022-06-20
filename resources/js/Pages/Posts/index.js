198691f290cb63eb29914e437fbbf1a3712f0b98
LorenzoWynberg <lwcincr93@gmail.com>
2022-06-20 10:22:21 -0600

    Pagination from API and React Sides

--- resources/js/Pages/Posts/index.js
+++ resources/js/Pages/Posts/index.js
@@ -1,18 +1,24 @@
-import axios from "axios";
-import { Component } from "react";
+import axios from "axios"
+import { Component } from "react"
 
 class PostIndex extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
 
 		this.state = {
 			posts: []
 		}
 	}
 
-	fetchPosts() {
-		axios.get('/api/posts')
-			.then(res => this.setState({ posts: res.data.data }))
+	fetchPosts(page = 1) {
+		axios.get('/api/posts', { params: { page } })
+			.then(res => this.setState({ posts: res.data }))
+	}
+
+	pageChanged(url) {
+		const fullUrl = new URL(url)
+		const page = fullUrl.searchParams.get('page')
+		this.fetchPosts(page)
 	}
 
 	componentDidMount() {
@@ -43,8 +49,8 @@ class PostIndex extends Component {
 	renderBody() {
 		return (
 			<tbody className="table-body">
-				{this.state.posts.map(
-					post => <tr>
+				{this.state.posts.data.map(
+					post => <tr key={post.id}>
 						<td>{post.id}</td>
 						<td>{post.title}</td>
 						<td>{post.content}</td>
@@ -55,7 +61,46 @@ class PostIndex extends Component {
 		)
 	}
 
+	renderPaginatorLinks() {
+		return this.state.posts.meta.links.map((link, index) =>
+			<button
+				key={index}
+				onClick={() => this.pageChanged(link.url)}
+				dangerouslySetInnerHTML={{ __html: link.label }}
+				className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 first:rounded-l-md last:rounded-r-md" />
+		);
+	}
+
+	renderPaginator() {
+		return (
+			<nav role="navigation" aria-label="Pagination Navigation" className="flex items-center justify-between">
+				<div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+					<div>
+						<p className="text-sm text-gray-700 leading-5">
+							Showing
+							<span>
+								<span className="font-medium"> {this.state.posts.meta.from} </span>
+								to
+								<span className="font-medium"> {this.state.posts.meta.to} </span>
+							</span>
+							of
+							<span className="font-medium"> {this.state.posts.meta.total} </span>
+							results
+						</p>
+					</div>
+
+					<div>
+						<span className="relative z-0 inline-flex shadow-sm rounded-md">
+							{this.renderPaginatorLinks()}
+						</span>
+					</div>
+				</div>
+			</nav>
+		);
+	}
+
 	render() {
+		if (!('data' in this.state.posts)) return;
 		return (
 			<div className="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
 				<div className="min-w-full align-middle">
@@ -63,6 +108,9 @@ class PostIndex extends Component {
 						{this.renderHead()}
 						{this.renderBody()}
 					</table>
+					<div className="mt-4">
+						{this.renderPaginator()}
+					</div>
 				</div>
 			</div>
 		)
