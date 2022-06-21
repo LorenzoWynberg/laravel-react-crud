05f1d82079ae9d9bfe1cb67e5dd8c375b31e2a42
LorenzoWynberg <lwcincr93@gmail.com>
2022-06-21 17:53:27 -0600

    Saving query params on page change and category change

--- resources/js/Pages/Posts/index.jsx
+++ resources/js/Pages/Posts/index.jsx
@@ -1,86 +1,78 @@
-import axios from "axios"
-import { Component } from "react"
+import axios from "axios";
+import { Component } from "react";
 
 class PostIndex extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
 
 		this.state = {
 			posts: [],
 			categories: [],
 			query: {
 				page: 1,
-				category_id: '',
-				order_column: 'id',
-				order_direction: 'desc',
-			}
-		}
-
-		this.categoryChanged = this.categoryChanged.bind(this);
-		this.pageChanged = this.pageChanged.bind(this);
-		this.orderByChanged = this.orderByChanged.bind(this);
+				category_id: "",
+				order_column: "id",
+				order_direction: "desc",
+			},
+		};
 	}
 
 	fetchPosts() {
-		console.log(this.state.query);
-		axios.get('/api/posts', { params: this.state.query })
-			.then(res => this.setState({ posts: res.data }))
+		axios
+			.get("/api/posts", { params: this.state.query })
+			.then(res => this.setState({ posts: res.data }));
 	}
 
 	fetchCategories() {
-		axios.get('/api/categories')
-			.then(res => this.setState({ categories: res.data.data }))
+		axios
+			.get("/api/categories")
+			.then(res => this.setState({ categories: res.data.data }));
 	}
 
-	pageChanged(url) {
-		const fullUrl = new URL(url)
-		this.setState(({
+	categoryChanged = (event) => {
+		this.setState(
+			{
+				query: {
+					category_id: event.target.value,
+					page: 1,
+					order_column: state.query.order_column,
+					order_direction: state.query.order_direction,
+				},
+			},
+			() => this.fetchPosts()
+		);
+	};
+
+	pageChanged = (url) => {
+		const page = new URL(url).searchParams.get('page');
+		this.setState((state) => ({
 			query: {
-				page: fullUrl.searchParams.get('page')
-			}
-		}), () => this.fetchPosts())
-	}
+				category_id: state.query.category_id,
+				page: page,
+				order_column: state.query.order_column,
+				order_direction: state.query.order_direction,
+			},
+		}), () => this.fetchPosts());
+	};
 
-	categoryChanged(event) {
-		this.setState(({
-			query: {
-				category_id: event.target.value,
-				page: 1
-			}
-		}), () => this.fetchPosts())
-	}
-
-	orderByColumnIcon(column) {
-		let icon = 'fa-sort';
-		if (this.state.query.order_column === column) {
-			icon = this.state.query.order_direction === 'asc' ?
-				icon = 'fa-sort-up' :
-				icon = 'fa-sort-down'
-		}
-		return (
-			<i className={`fa-solid ${icon}`}></i>
-		)
-	}
-
-	orderByChanged(column) {
-		let direction = 'asc';
+	orderByChanged = (column) => {
+		let direction = "asc";
 		if (column === this.state.query.order_column) {
-			direction = this.state.query.order_direction === 'asc' ?
-				'desc' :
-				'asc'
+			direction = this.state.query.order_direction === "asc" ? "desc" : "asc";
 		}
-		this.setState(({
+		this.setState((state) => ({
 			query: {
 				page: 1,
 				order_column: column,
-				order_direction: direction
-			}
-		}), () => this.fetchPosts())
-	}
+				order_direction: direction,
+				category_id: state.query.category_id,
+			},
+		}), () => this.fetchPosts());
+	};
 
 	componentDidMount() {
-		this.fetchPosts()
-		this.fetchCategories()
+		this.fetchPosts();
+		this.fetchCategories();
 	}
 
 	renderHead() {
@@ -90,24 +82,33 @@ class PostIndex extends Component {
 					<th>
 						<div>
 							<span>ID</span>
-							<button onClick={() => this.orderByChanged('id')} type="button" className="column-sort">
-								{this.orderByColumnIcon('id')}
+							<button
+								onClick={() => this.orderByChanged("id")}
+								type="button"
+								className="column-sort">
+								{this.renderSortIcon("id")}
 							</button>
 						</div>
 					</th>
 					<th>
 						<div>
 							<span>Title</span>
-							<button onClick={() => this.orderByChanged('title')} type="button" className="column-sort">
-								{this.orderByColumnIcon('title')}
+							<button
+								onClick={() => this.orderByChanged("title")}
+								type="button"
+								className="column-sort">
+								{this.renderSortIcon("title")}
 							</button>
 						</div>
 					</th>
 					<th>
 						<div>
 							<span>Category</span>
-							<button onClick={() => this.orderByChanged('category_id')} type="button" className="column-sort">
-								{this.orderByColumnIcon('category_id')}
+							<button
+								onClick={() => this.orderByChanged("category_id")}
+								type="button"
+								className="column-sort">
+								{this.renderSortIcon("category_id")}
 							</button>
 						</div>
 					</th>
@@ -123,49 +124,70 @@ class PostIndex extends Component {
 					</th>
 				</tr>
 			</thead>
-		)
+		);
 	}
 
 	renderBody() {
 		return (
 			<tbody className="table-body">
-				{this.state.posts.data.map(
-					post => <tr key={post.id}>
+				{this.state.posts.data.map((post) => (
+					<tr key={post.id}>
 						<td>{post.id}</td>
 						<td>{post.title}</td>
 						<td>{post.category.name}</td>
 						<td>{post.content}</td>
 						<td>{post.created_at}</td>
 					</tr>
-				)}
+				))}
 			</tbody>
-		)
+		);
+	}
+
+	renderSortIcon(column) {
+		let icon = "fa-sort";
+		if (this.state.query.order_column === column) {
+			icon =
+				this.state.query.order_direction === "asc"
+					? (icon = "fa-sort-up")
+					: (icon = "fa-sort-down");
+		}
+		return <i className={`fa-solid ${icon}`}></i>;
 	}
 
 	renderPaginatorLinks() {
-		return this.state.posts.meta.links.map((link, index) =>
+		return this.state.posts.meta.links.map((link, index) => (
 			<button
 				key={index}
 				onClick={() => this.pageChanged(link.url)}
 				dangerouslySetInnerHTML={{ __html: link.label }}
-				className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 first:rounded-l-md last:rounded-r-md" />
-		);
+				className="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 first:rounded-l-md last:rounded-r-md"
+			/>
+		));
 	}
 
 	renderPaginator() {
 		return (
-			<nav role="navigation" aria-label="Pagination Navigation" className="flex items-center justify-between">
+			<nav
+				role="navigation"
+				aria-label="Pagination Navigation"
+				className="flex items-center justify-between">
 				<div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
 					<div>
 						<p className="text-sm text-gray-700 leading-5">
 							Showing
 							<span>
-								<span className="font-medium"> {this.state.posts.meta.from} </span>
+								<span className="font-medium">
+									{this.state.posts.meta.from}
+								</span>
 								to
-								<span className="font-medium"> {this.state.posts.meta.to} </span>
+								<span className="font-medium">
+									{this.state.posts.meta.to}
+								</span>
 							</span>
 							of
-							<span className="font-medium"> {this.state.posts.meta.total} </span>
+							<span className="font-medium">
+								{this.state.posts.meta.total}
+							</span>
 							results
 						</p>
 					</div>
@@ -181,38 +203,36 @@ class PostIndex extends Component {
 	}
 
 	renderCategoryFilter() {
-		const categories = this.state.categories.map(category =>
-			<option key={category.id} value={category.id}>{category.name}</option>
-		);
+		const categories = this.state.categories.map((category) => (
+			<option key={category.id} value={category.id}>
+				{category.name}
+			</option>
+		));
 
 		return (
 			<select
 				onChange={this.categoryChanged}
 				className="mt-1 w-full sm:mt-0 sm:w-1/4 rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-				<option>-- all categories --</option>
+				<option value="">-- all categories --</option>
 				{categories}
 			</select>
-		)
+		);
 	}
 
 	render() {
-		if (!('data' in this.state.posts)) return;
+		if (!("data" in this.state.posts)) return;
 		return (
 			<div className="overflow-hidden overflow-x-auto p-6 bg-white border-gray-200">
 				<div className="min-w-full align-middle">
-					<div className="mb-4">
-						{this.renderCategoryFilter()}
-					</div>
+					<div className="mb-4">{this.renderCategoryFilter()}</div>
 					<table className="table">
 						{this.renderHead()}
 						{this.renderBody()}
 					</table>
-					<div className="mt-4">
-						{this.renderPaginator()}
-					</div>
+					<div className="mt-4">{this.renderPaginator()}</div>
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
