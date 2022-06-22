c84dc07164aeec5c34fd8f3723137f8bc3d23365
LorenzoWynberg <lwcincr93@gmail.com>
2022-06-22 17:36:52 -0600

    Created NavLink component

--- resources/js/layouts/Nav/MainNav.jsx
+++ resources/js/layouts/Nav/MainNav.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import NavLink from "./NavLink";
 
 export default function MainNav() {
 	return (
@@ -12,22 +12,8 @@ export default function MainNav() {
 							</a>
 						</div>
 						<div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-							<NavLink to="/"
-								className={
-									({ isActive }) => (isActive
-										? "border-indigo-400 text-gray-900 focus:border-indigo-700 "
-										: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 ")
-										+ "focus:outline-none text-sm font-medium leading-5 inline-flex items-center px-1 pt-1 border-b-2 transition duration-150 ease-in-out"}>
-								Posts
-							</NavLink>
-							<NavLink to="/posts/create"
-								className={
-									({ isActive }) => (isActive
-										? "border-indigo-400 text-gray-900 focus:border-indigo-700 "
-										: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 ")
-										+ "focus:outline-none text-sm font-medium leading-5 inline-flex items-center px-1 pt-1 border-b-2 transition duration-150 ease-in-out"}>
-								Add Post
-							</NavLink>
+							<NavLink route={"/"} name={"Posts"} />
+							<NavLink route={"/posts/create"} name={"Add Post"} />
 						</div>
 					</div>
 				</div>
---
+++ resources/js/layouts/Nav/NavLink.jsx
@@ -0,0 +1,14 @@
+import { NavLink as Link } from "react-router-dom";
+
+export default function NavLink({ route, name }) {
+	return (
+		<Link to={route}
+			className={
+				({ isActive }) => (isActive
+					? "border-indigo-400 text-gray-900 focus:border-indigo-700 "
+					: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 ")
+					+ "focus:outline-none text-sm font-medium leading-5 inline-flex items-center px-1 pt-1 border-b-2 transition duration-150 ease-in-out"}>
+			{name}
+		</Link>
+	)
+}
