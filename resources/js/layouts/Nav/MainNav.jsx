897cbb8c44f3b4b17c066c1ba37ec46fb1819092
LorenzoWynberg <lwcincr93@gmail.com>
2022-06-22 17:26:11 -0600

    Routing: Active links and conditional classes

--- resources/js/layouts/Nav/MainNav.jsx
+++ resources/js/layouts/Nav/MainNav.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function MainNav() {
 	return (
@@ -11,13 +11,23 @@ export default function MainNav() {
 								React App
 							</a>
 						</div>
-
 						<div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-							<Link to="/"
-								className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">Posts</Link>
-							<Link to="/posts/create"
-								className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">Add
-								Post</Link>
+							<NavLink to="/"
+								className={
+									({ isActive }) => (isActive
+										? "border-indigo-400 text-gray-900 focus:border-indigo-700 "
+										: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 ")
+										+ "focus:outline-none text-sm font-medium leading-5 inline-flex items-center px-1 pt-1 border-b-2 transition duration-150 ease-in-out"}>
+								Posts
+							</NavLink>
+							<NavLink to="/posts/create"
+								className={
+									({ isActive }) => (isActive
+										? "border-indigo-400 text-gray-900 focus:border-indigo-700 "
+										: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 ")
+										+ "focus:outline-none text-sm font-medium leading-5 inline-flex items-center px-1 pt-1 border-b-2 transition duration-150 ease-in-out"}>
+								Add Post
+							</NavLink>
 						</div>
 					</div>
 				</div>
