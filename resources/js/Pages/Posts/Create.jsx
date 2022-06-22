d2bf18e32462865f77b9459bc829668b4023d004
LorenzoWynberg <lwcincr93@gmail.com>
2022-06-22 14:11:39 -0600

    Added Initial Create Post Page

---
+++ resources/js/Pages/Posts/Create.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import MainLayout from "@layouts/MainLayout/MainLayout"
+
+class PostsCreate extends Component {
+	render() {
+		return (
+			<MainLayout title={"Create Post"}>
+				<div>Create form will be here.</div>
+			</MainLayout>
+		)
+	}
+}
+
+export default PostsCreate;
--- resources/js/routes/Main.jsx
+++ resources/js/routes/Main.jsx
@@ -5,12 +5,14 @@ import {
 	Route,
 } from "react-router-dom";
 import PostsIndex from "@pages/Posts";
+import PostsCreate from "@pages/Posts/Create";
 
 function Main() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<PostsIndex />}></Route>
+				<Route path="/posts/create" element={<PostsCreate />}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
