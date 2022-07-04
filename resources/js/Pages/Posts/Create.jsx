d555a0209995bf6ece4eafb63b75c3b8cca6bea7
LorenzoWynberg <lwcincr93@gmail.com>
2022-07-04 09:44:46 -0600

    Validating post create

--- resources/js/Pages/Posts/Create.jsx
+++ resources/js/Pages/Posts/Create.jsx
@@ -7,6 +7,7 @@ function PostCreate() {
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
 	const [categoryId, setCategoryId] = useState('');
+	const [errors, setErrors] = useState({});
 
 	const navigate = useNavigate()
 	const submit = (event) => {
@@ -17,8 +18,22 @@ function PostCreate() {
 				content: content,
 				category_id: categoryId,
 			})
-			.then(navigate('/'))
-			.catch(e => console.log(e))
+			.then(res => navigate('/'))
+			.catch(e => setErrors(e.response.data.errors))
+	}
+
+	const errorMessage = (field) => {
+		return (
+			<div className="text-red-600 mt-1">
+				{
+					errors?.[field]?.map((message, index) => {
+						return (
+							<div key={index}>{message}</div>
+						)
+					})
+				}
+			</div>
+		)
 	}
 
 	const inputClassList = "block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -32,18 +47,21 @@ function PostCreate() {
 						Title
 					</label>
 					<input value={title} onChange={(e) => { setTitle(e.target.value) }} name="title" type="text" className={inputClassList} />
+					{errorMessage('title')}
 				</div>
 				<div className="mt-4">
 					<label htmlFor="content" className={labelClassList}>
 						Content
 					</label>
 					<textarea value={content} onChange={(e) => { setContent(e.target.value) }} name="content" type="text" className={inputClassList} />
+					{errorMessage('content')}
 				</div>
 				<div className="mt-4">
-					<label htmlFor="category" className={labelClassList}>
+					<label htmlFor="category_id" className={labelClassList}>
 						Category
 					</label>
 					<CategorySelect callback={(e) => { setCategoryId(e.target.value) }} />
+					{errorMessage('category_id')}
 				</div>
 				<div className="mt-4">
 					<button type="submit" className="px-3 py-2 bg-indigo-500 text-white rounded">
